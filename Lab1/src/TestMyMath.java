889f1f5b6bb5bbf7ceee0e2d4a4e8713f91efaa4
mingxuan8833 <mingxuan8833@gmail.com>
2023-02-11 21:47:06 +0800

    Done Lab1

--- Lab1/src/TestMyMath.java
+++ Lab1/src/TestMyMath.java
@@ -1,5 +1,7 @@
 public class TestMyMath {
     public static void main(String[] args){
         System.out.println(MyMath.SumofNumber(5));
+        System.out.println(MyMath.power(2,2));
+        System.out.println(MyMath.smallest(4,5,6));
     }
 }
