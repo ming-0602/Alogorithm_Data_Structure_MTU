0b99d8a1f1c334d476d2397654c7e51602ed91ba
mingxuan8833 <mingxuan8833@gmail.com>
2023-02-20 05:53:53 +0800

    Done Lab2

---
+++ Lab2/src/Encrypter.java
@@ -0,0 +1,64 @@
+import java.util.Locale;
+
+public class Encrypter {
+
+    public static String encryptRowColumn(String plaintext, int numColumns){
+        plaintext = plaintext.replaceAll(" ", "").toUpperCase();
+        int numRow = Math.ceilDiv(plaintext.length(),numColumns);
+
+        if(plaintext.length() < numRow * numColumns){
+            while (plaintext.length() < numRow * numColumns){
+                StringBuilder str = new StringBuilder(plaintext);
+                str.append("X");
+                plaintext = str.toString();
+            }
+        }
+
+        StringBuilder Cipertext = new StringBuilder();
+        for (int col = 0; col < numColumns; col++){
+            int index = col;
+            for (int row = 0; row < numRow; row++){
+                Cipertext.append(plaintext.toCharArray()[index]);
+                index += numColumns;
+            }
+        }
+
+        return Cipertext.toString();
+    }
+
+
+    public static String decryptRowColumn(String plaintext, int numColumns){
+        int Key = Math.ceilDiv(plaintext.length(),numColumns);
+        return encryptRowColumn(plaintext,Key);
+    }
+
+
+
+    public static String encryptCaesarCipher(String plaintext, int shift){
+
+        if(shift >= 26) shift/=26;
+
+        char[] chararr = plaintext.replaceAll(" ", "").toUpperCase().toCharArray();
+        StringBuilder cipertext = new StringBuilder();
+        for (char chr : chararr) {
+            cipertext.append((char) (chr + shift));
+        }
+
+        return cipertext.toString();
+    }
+
+
+    public static String decryptCaesarCipher(String plaintext, int shift){
+
+        if(shift >= 26) shift/=26;
+
+        char[] chararr = plaintext.replaceAll(" ", "").toUpperCase().toCharArray();
+        StringBuilder cipertext = new StringBuilder();
+        for (char chr : chararr) {
+            cipertext.append((char) (chr - shift));
+        }
+
+        return cipertext.toString();
+
+    }
+}
---
+++ Lab2/src/TestEncrypter.java
@@ -0,0 +1,11 @@
+public class TestEncrypter {
+    public static void main(String[] args) {
+        System.out.println(Encrypter.encryptRowColumn("Secret Message", 3));
+        System.out.println(Encrypter.decryptRowColumn("SRMSEEEEAXCTSGX", 3));
+
+
+
+        System.out.println(Encrypter.encryptCaesarCipher("ABCD", 3));
+        System.out.println(Encrypter.decryptCaesarCipher("DEFG", 3));
+    }
+}
